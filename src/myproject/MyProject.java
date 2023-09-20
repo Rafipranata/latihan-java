c86f4708257f548396e43644f56e1bf1d8ca3b5b
Rafipranata <banjardawa34@gmail.com>
2023-09-20 13:58:18 +0800

    first commit

---
+++ src/myproject/MyProject.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject;
+
+/**
+ *
+ * @author PC13
+ */
+public class MyProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+        int rafi = 10;
+        System.out.println(rafi);
+        
+        
+    }
+    
+}
